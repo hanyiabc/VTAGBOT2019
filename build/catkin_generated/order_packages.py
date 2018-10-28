b31755ef56ddc7169340d498f3ae6e8aacf0b6e3
Howard <hongxug5@gmail.com>
2018-10-28 16:23:34 -0400

    added libStaticMapPlugin

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/hongxu/VTAGBOT2019/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/hongxu/VTAGBOT2019/devel;/opt/ros/melodic".split(';') if "/home/hongxu/VTAGBOT2019/devel;/opt/ros/melodic" != "" else []
+underlay_workspaces = "/opt/ros/melodic".split(';') if "/opt/ros/melodic" != "" else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/hongxu/VTAGBOT2019/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/hongxu/VTAGBOT2019/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/hongxu/VTAGBOT2019/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/hongxu/VTAGBOT2019/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/hongxu/VTAGBOT2019/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/hongxu/VTAGBOT2019/src:$ROS_PACKAGE_PATH"
